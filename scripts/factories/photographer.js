12b504d44b2e14ee6b8f3d5414a89cf1f207a26c
michelD-dev <delaunaymichel@hotmail.fr>
2022-03-31 18:44:16 +0200

    Transformation css en scss

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -17,30 +17,35 @@ function photographerFactory(data) {
 
     const img = document.createElement('img')
     img.setAttribute('src', picture)
-    img.setAttribute('alt', `${phoName} - Fiche individuelle`)//FIXME vérifier si ce sera un lien
+    img.setAttribute('alt', `${phoName} - Fiche individuelle`)
     img.classList.add('photographer__portrait')
- 
+
     const name = document.createElement('h2')
     name.textContent = phoName
     name.classList.add('photographer__name')
     name.setAttribute('aria-label', 'Nom du photographe')
-    
+
+    const articleHeader = document.createElement('a')
+    articleHeader.setAttribute('href', '#')
+    articleHeader.classList.add('photographer-card__header')
+    articleHeader.appendChild(img)
+    articleHeader.appendChild(name)
+
     const location = document.createElement('span')
     location.textContent = `${city}, ${country}`
     location.classList.add('photographer__location')
     location.setAttribute('aria-label', 'Localisation du photographe')
-    
+
     const tagline = document.createElement('span')
     tagline.textContent = tag
-    tagline.setAttribute('aria-label', 'Citation du photographe')
-    
+    tagline.setAttribute('aria-label', 'Slogan du photographe')
+
     const price = document.createElement('span')
     price.textContent = `${rate}€/jour`
     price.classList.add('photographer__price')
     price.setAttribute('aria-label', 'Tarif journalier')
 
-    article.appendChild(img)
-    article.appendChild(name)
+    article.appendChild(articleHeader)
     article.appendChild(location)
     article.appendChild(tagline)
     article.appendChild(price)
