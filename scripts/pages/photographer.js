03c1617a10e3a851ad3c5d4c6b72097635bfe267
MichelD-dev <delaunaymichel@hotmail.fr>
2022-08-21 02:17:04 +0200

    Fix maintien affichage like lors du changement d'ordre de tri

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -94,71 +94,75 @@ mutationObserver.observe(DOM.mediasSection, {
 /**
  * Récupération d'un photographe et des médias associés par critère de tri
  */
-export const getMediasSorting =
-  (photographers) => (medias) => (sortingChoice) => {
-    /**
-     * Définition du photographe d'après son id
-     */
-    const photographer = photographers.find(
-      (photographer) => photographer.id === id
-    );
+export const getMediasSorting = (photographers) => (medias) => {
+  /**
+   * Définition du photographe d'après son id
+   */
+  const photographer = photographers.find(
+    (photographer) => photographer.id === id
+  );
 
-    /**
-     * Filtrage des données selon le photographe
-     */
-    const photographerMedias = medias.filter(
-      (media) => media.photographerId === photographer.id
-    );
+  /**
+   * Filtrage des données selon le photographe
+   */
+  const photographerMedias = medias.filter(
+    (media) => media.photographerId === photographer.id
+  );
 
-    /**
-     * Tri de l'ordre d'affichage des images selon choix utilisateur
-     */
-    const sortedPhotographerMedias = sortBy(photographerMedias)(sortingChoice);
-
-    return { photographer, sortedPhotographerMedias };
-  };
+  return { photographer, photographerMedias };
+};
 
 /**
  * Récupération des données photographes/médias, par popularité par défaut
  */
-const getDatas = async () => {
-  /**
-   * Récupération de l'ensemble des data
-   */
-  const { photographers, medias } = await getFetchedDatas({
-    url: "./data/photographers.json",
-    storageName: "original datas",
-  });
 
-  return { photographers, medias };
-};
-
-const { photographers, medias } = await getDatas();
+/**
+ * Récupération de l'ensemble des data
+ */
+const { photographers, medias } = await getFetchedDatas({
+  url: "./data/photographers.json",
+  storageName: "original datas",
+});
 
 /**
  * Récupération d'un photographe spécifique et des médias associés par critère de tri
  */
+const { photographer: forThisPhotographer, photographerMedias } =
+  getMediasSorting(photographers)(medias);
+
+/**
+ * Stockage de toutes les images du photographe dans le local storage
+ */
+let likedMedias = [];
+
+photographerMedias.forEach((media) => {
+  likedMedias = [
+    ...likedMedias,
+    { id: media.id, likes: media.likes, isLiked: false },
+  ];
+  store.setLikedImages(likedMedias);
+});
+
 export const sort = (sortingChoice = "Popularité") => {
-  const { photographer: forThisPhotographer, sortedPhotographerMedias } =
-    getMediasSorting(photographers)(medias)(sortingChoice);
-
   /**
-   * Stockage de toutes les images du photographe dans le local storage
+   * Tri de l'ordre d'affichage des images selon choix utilisateur
    */
-  let likedMedias = [];
-
-  sortedPhotographerMedias.forEach((media) => {
-    likedMedias = [
-      ...likedMedias,
-      { id: media.id, likes: media.likes, isLiked: false },
-    ];
-    store.setLikedImages(likedMedias);
-  });
+  const sortedPhotographerMedias = sortBy(photographerMedias)(sortingChoice);
 
   /**
    * Affichage des médias
    */
   displayMedias(forThisPhotographer)(sortedPhotographerMedias);
+
+  /**
+   * Maintien de l'affichage du like au changement d'ordre de tri
+   */
+  mutationObserver.observe(
+    [...DOM.mediasSection.getElementsByClassName("media-card")][0],
+    {
+      attributeFilter: ["class"],
+    }
+  );
 
   /**
    * On récupère les éléments qui acquerront le focus
@@ -175,7 +179,6 @@ export const sort = (sortingChoice = "Popularité") => {
    * Changement de focus au clavier et maintien du focus dans la fenêtre
    */
   const focusInWindow = (e) => {
-    console.log(document.querySelector(":focus"));
     e.preventDefault();
     let index = focusables.findIndex(
       (elem) => elem === document.querySelector(":focus")
